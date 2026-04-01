5fe69e56c924a4bc7e936c97419a61a636526154
iaroslava315 <iaroslava.315@gmail.com>
2026-04-01 13:54:54 +0700

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        int age = 17;
+        int age = 2;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
         }
         else {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
         }
+
 
 
         int temperature = 10;
@@ -23,6 +24,7 @@ public class Main {
         }
 
 
+
         int speed = 59;
         if (speed < 60) {
             System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно");
@@ -30,6 +32,27 @@ public class Main {
         else {
             System.out.println("Если скорость " + speed + " км/ч, то придется заплатить штраф");
         }
+
+
+
+        boolean goToKindergarten = age >= 2 && age <= 6;
+        if (goToKindergarten) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+        }
+        boolean goToSchool = age >= 7 && age <= 17;
+        if (goToSchool) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+        }
+        boolean goToUniversity = age >= 18 && age <= 24;
+        if (goToUniversity) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет");
+        }
+        boolean goToWork = age > 24;
+        if (goToWork) {
+            System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу");
+        }
+
+
 
 
         }
